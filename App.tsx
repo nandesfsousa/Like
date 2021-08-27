f62271f0c51311606054f78f0799fb33b9a03d81
nandesfsousa <nandesfsousa@gmail.com>
2021-08-27 15:11:42 -0400

    background color and horizontal scroll

--- App.tsx
+++ App.tsx
@@ -1,21 +1,183 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SectionList,
+  SafeAreaView,
+  Image,
+  FlatList
+} from 'react-native';
 
+const ListItem = ({ item }) => {
+  return (
+    <View style={styles.item}>
+      <Image
+        source={{
+          uri: item.uri,
+        }}
+        style={styles.itemPhoto}
+        resizeMode="cover"
+      />
+      <Text style={styles.itemText}>{item.text}</Text>
+    </View>
+  );
+};
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+      <SafeAreaView style={{ flex: 1 }}>
+        <SectionList
+          contentContainerStyle={{ paddingHorizontal: 10 }}
+          stickySectionHeadersEnabled={false}
+          sections={SECTIONS}
+          renderSectionHeader={({ section }) => (
+            <>
+              <Text style={styles.sectionHeader}>{section.title}</Text>
+              {section.horizontal ? (
+                <FlatList
+                  horizontal
+                  data={section.data}
+                  renderItem={({ item }) => <ListItem item={item} />}
+                  showsHorizontalScrollIndicator={false}
+                />
+              ) : null}
+            </>
+          )}
+          renderItem={({ item, section }) => {
+            if (section.horizontal) {
+              return null;
+            }
+            return <ListItem item={item} />;
+          }}
+        />
+      </SafeAreaView>
     </View>
   );
 }
 
+const SECTIONS = [
+  {
+    title: 'Lugares',
+    horizontal: true,
+    data: [
+      {
+        key: '1',
+        text: 'Item text 1',
+        uri: 'https://picsum.photos/id/1/200',
+      },
+      {
+        key: '2',
+        text: 'Item text 2',
+        uri: 'https://picsum.photos/id/10/200',
+      },
+
+      {
+        key: '3',
+        text: 'Item text 3',
+        uri: 'https://picsum.photos/id/1002/200',
+      },
+      {
+        key: '4',
+        text: 'Item text 4',
+        uri: 'https://picsum.photos/id/1006/200',
+      },
+      {
+        key: '5',
+        text: 'Item text 5',
+        uri: 'https://picsum.photos/id/1008/200',
+      },
+    ],
+  },
+  {
+    title: 'Produtos',
+    horizontal: true,
+    data: [
+      {
+        key: '1',
+        text: 'Item text 1',
+        uri: 'https://picsum.photos/id/1011/200',
+      },
+      {
+        key: '2',
+        text: 'Item text 2',
+        uri: 'https://picsum.photos/id/1012/200',
+      },
+
+      {
+        key: '3',
+        text: 'Item text 3',
+        uri: 'https://picsum.photos/id/1013/200',
+      },
+      {
+        key: '4',
+        text: 'Item text 4',
+        uri: 'https://picsum.photos/id/1015/200',
+      },
+      {
+        key: '5',
+        text: 'Item text 5',
+        uri: 'https://picsum.photos/id/1016/200',
+      },
+    ],
+  },
+  {
+    title: 'Pessoas',
+    horizontal: true,
+    data: [
+      {
+        key: '1',
+        text: 'Item text 1',
+        uri: 'https://picsum.photos/id/1020/200',
+      },
+      {
+        key: '2',
+        text: 'Item text 2',
+        uri: 'https://picsum.photos/id/1024/200',
+      },
+
+      {
+        key: '3',
+        text: 'Item text 3',
+        uri: 'https://picsum.photos/id/1027/200',
+      },
+      {
+        key: '4',
+        text: 'Item text 4',
+        uri: 'https://picsum.photos/id/1035/200',
+      },
+      {
+        key: '5',
+        text: 'Item text 5',
+        uri: 'https://picsum.photos/id/1038/200',
+      },
+    ],
+  },
+];
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#23b5d3ff',
+  },
+  sectionHeader: {
+    fontWeight: '800',
+    fontSize: 18,
+    color: '#000',
+    marginTop: 20,
+    marginBottom: 5,
+  },
+  item: {
+    margin: 10,
+  },
+  itemPhoto: {
+    width: 200,
+    height: 200,
+  },
+  itemText: {
+    color: 'rgba(255, 255, 255, 0.5)',
+    marginTop: 5,
   },
 });
